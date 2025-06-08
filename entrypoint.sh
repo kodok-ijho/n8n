b229d8281df1145d45262990133c9e8d9c315d68
kodok-ijho <dyudhiantoro@gmail.com>
2025-06-08 14:40:42 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
 echo "Installing MCP plugin..."
 npm install --unsafe-perm=true --allow-root n8n-nodes-mcp
+
+echo "Starting WAHA..."
+node /waha/index.js &
 
 echo "Starting n8n..."
 exec n8n
